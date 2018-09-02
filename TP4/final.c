7ed42ed65b4f64277d8be35585ab38d17b4b6ddf
Ricardo <xlin@itba.edu.ar>
2018-09-02 20:52:29 -0300

    subo cambios

--- TP4/final.c
+++ TP4/final.c
@@ -88,7 +88,7 @@ int evaluate (char matrx[100][10], int i, int j, int fila, int col) {
   for (n=-1;n<=1;n++) { //evaluamos las posiciones de las filas anterior, misma y sig
     for (h=-1;h<=1;h++) { //evaluamos las posiciones de las columnas anterior, misma y sig
       if (matrx[i+n][j+h]=='X') {
-        if ( ((i+n)>=0) && ((i+n)<=fila) && ((j+h)>=0) && ((j+h)<=col) ) {  //la particula a evaluar debe estar dentro del rango
+        if ( ((i+n)>=0) && ((i+n)<fila) && ((j+h)>=0) && ((j+h)<col) ) {  //la particula a evaluar debe estar dentro del rango
           total++;  //sumamos uno al total
         }
       }
