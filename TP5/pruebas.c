01e1f1e3832325d88dccd68d7d9ddfdf8892b9e7
Lucas Vidmar <lvidmar@itba.edu.ar>
2018-09-09 18:47:02 -0300

    testeando

--- TP5/pruebas.c
+++ TP5/pruebas.c
@@ -5,14 +5,17 @@
 
 int main(void) {
   int i;
-  char ch, array[200];
+  char ch, array[200], palabra[200];
 
   for (i=0; (ch=getchar())!= EOF ;i++) {
     array[i]=ch;
   }
 
   for (i=0; (array[i]!=TER) ;i++) {
-    printf("%c\n",ch);
+    printf("%c",array[i]);
   }
 
+  for (i=0; (ch=getchar())!=TER ;i++) {
+    palabra[i]=ch;
+  }
 }
